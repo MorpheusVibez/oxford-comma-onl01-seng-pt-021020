9d3db21b3cdf3c7bde8c36f5de985c4ad9e4c14b
Andre Skeene <halodre24@gmail.com>
2020-02-17 01:29:43 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,7 @@ def oxford_comma(array)
       ", ")
   elsif array.size == 2 
     array.join(" and ")
-  elsif array.size == 3
-    array.join 
+  else 
+    return array[0..-2].join(", ") + ", and " + array[-1]
   end
 end
